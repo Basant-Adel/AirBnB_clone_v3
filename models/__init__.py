21f262e17e5a0ca6695254b75c8bacaeb76ee167
salihabrik <briksaliha@gmail.com>
2023-10-31 00:17:07 +0100

    update

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,6 @@ initialize the models package
 
 from os import getenv
 
-storage = None
 storage_t = getenv("HBNB_TYPE_STORAGE")
 
 if storage_t == "db":
--- models/place.py
+++ models/place.py
@@ -6,9 +6,6 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
-from api.v1.views import app_views
-from models import storage
-from flask import Flask, jsonify, abort, make_response, request
 
 if models.storage_t == 'db':
     place_amenity = Table('place_amenity', Base.metadata,
